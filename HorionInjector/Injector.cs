46010298158041059cae40e155450a85453b0f49
richard <mail@richard.yt>
2021-08-23 02:44:42 +0200

    unclutter code and and implement connectionstate

---
+++ HorionInjector/Injector.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.VisualBasic;
+
+namespace HorionInjector
+{
+    partial class MainWindow
+    {
+        // IMPORTS
+        [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+        public static extern IntPtr OpenProcess(IntPtr dwDesiredAccess, bool bInheritHandle, uint processId);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern bool CloseHandle(IntPtr hObject);
+
+        [DllImport("kernel32.dll", ExactSpelling = true, SetLastError = true)]
+        public static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, char[] lpBuffer, int nSize, out IntPtr lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+        public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, ref IntPtr lpThreadId);
+
+        [DllImport("kernel32", SetLastError = true)]
+        public static extern uint WaitForSingleObject(IntPtr handle, uint milliseconds);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool VirtualFreeEx(IntPtr hProcess, IntPtr lpAddress, int dwSize, IntPtr dwFreeType);
+
+        [DllImport("user32.DLL", CharSet = CharSet.Unicode)]
+        public static extern IntPtr FindWindow(String lpClassName, String lpWindowName);
+
+        [DllImport("user32.DLL")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+        //
+
+        private void Inject(string path)
+        {
+            if (!File.Exists(path))
+            {
+                MessageBox.Show("DLL not found, your Antivirus might have deleted it.");
+                goto done;
+            }
+
+            if (File.ReadAllBytes(path).Length < 10)
+            {
+                MessageBox.Show("DLL broken (Less than 10 bytes)");
+                goto done;
+            }
+
+
+            SetStatus("setting file perms");
+            try
+            {
+                var fileInfo = new FileInfo(path);
+                var accessControl = fileInfo.GetAccessControl();
+                accessControl.AddAccessRule(new FileSystemAccessRule(new SecurityIdentifier("S-1-15-2-1"), FileSystemRights.FullControl, InheritanceFlags.None, PropagationFlags.NoPropagateInherit, AccessControlType.Allow));
+                fileInfo.SetAccessControl(accessControl);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Could not set permissions, try running the injector as admin.");
+                goto done;
+            }
+
+            SetStatus("finding process");
+            var processes = Process.GetProcessesByName("Minecraft.Windows");
+            if (processes.Length == 0)
+            {
+                SetStatus("launching minecraft");
+                if (Interaction.Shell("explorer.exe shell:appsFolder\\Microsoft.MinecraftUWP_8wekyb3d8bbwe!App", Wait: false) == 0)
+                {
+                    MessageBox.Show("Failed to launch Minecraft (Is it installed?)");
+                    goto done;
+                }
+
+                Task.Run(() =>
+                {
+                    int t = 0;
+                    while (processes.Length == 0)
+                    {
+                        if (++t > 200)
+                        {
+                            MessageBox.Show("Minecraft launch took too long.");
+                            return;
+                        }
+
+                        processes = Process.GetProcessesByName("Minecraft.Windows");
+                        Thread.Sleep(10);
+                    }
+                    Thread.Sleep(100);
+                }).Wait();
+            }
+            var process = processes.OrderBy(p => p.StartTime).First();
+
+            for (int i = 0; i < process.Modules.Count; i++)
+            {
+                if (process.Modules[i].FileName == path)
+                {
+                    MessageBox.Show("Already injected!");
+                    goto done;
+                }
+            }
+
+            SetStatus("injecting into " + process.Id);
+            IntPtr handle = OpenProcess((IntPtr)2035711, false, (uint)process.Id);
+            if (handle == IntPtr.Zero || !process.Responding)
+            {
+                MessageBox.Show("Failed to get process handle");
+                goto done;
+            }
+
+            IntPtr p1 = VirtualAllocEx(handle, IntPtr.Zero, (uint)(path.Length + 1), 12288U, 64U);
+            WriteProcessMemory(handle, p1, path.ToCharArray(), path.Length, out IntPtr p2);
+            IntPtr procAddress = GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
+            IntPtr p3 = CreateRemoteThread(handle, IntPtr.Zero, 0U, procAddress, p1, 0U, ref p2);
+            if (p3 == IntPtr.Zero)
+            {
+                MessageBox.Show("Failed to create remote thread");
+                goto done;
+            }
+
+            uint n = WaitForSingleObject(p3, 5000);
+            if (n == 128L || n == 258L)
+            {
+                CloseHandle(p3);
+            }
+            else
+            {
+                VirtualFreeEx(handle, p1, 0, (IntPtr)32768);
+                if (p3 != IntPtr.Zero)
+                    CloseHandle(p3);
+                if (handle != IntPtr.Zero)
+                    CloseHandle(handle);
+            }
+
+            IntPtr windowH = FindWindow(null, "Minecraft");
+            if (windowH == IntPtr.Zero)
+                Console.WriteLine("Couldn't get window handle");
+            else
+                SetForegroundWindow(windowH);
+
+            done: SetStatus("done");
+        }
+    }
+}
--- HorionInjector/MainWindow.xaml.cs
+++ HorionInjector/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.NetworkInformation;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Security.AccessControl;
 using System.Security.Principal;
@@ -23,41 +24,6 @@ namespace HorionInjector
     /// </summary>
     public partial class MainWindow
     {
-        // IMPORTS
-        [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
-        public static extern IntPtr OpenProcess(IntPtr dwDesiredAccess, bool bInheritHandle, uint processId);
-
-        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        public static extern bool CloseHandle(IntPtr hObject);
-
-        [DllImport("kernel32.dll", ExactSpelling = true, SetLastError = true)]
-        public static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, char[] lpBuffer, int nSize, out IntPtr lpNumberOfBytesWritten);
-
-        [DllImport("kernel32.dll", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
-        public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
-
-        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        public static extern IntPtr GetModuleHandle(string lpModuleName);
-
-        [DllImport("kernel32.dll")]
-        public static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, ref IntPtr lpThreadId);
-
-        [DllImport("kernel32", SetLastError = true)]
-        public static extern uint WaitForSingleObject(IntPtr handle, uint milliseconds);
-
-        [DllImport("kernel32.dll")]
-        public static extern bool VirtualFreeEx(IntPtr hProcess, IntPtr lpAddress, int dwSize, IntPtr dwFreeType);
-
-        [DllImport("user32.DLL", CharSet = CharSet.Unicode)]
-        public static extern IntPtr FindWindow(String lpClassName, String lpWindowName);
-
-        [DllImport("user32.DLL")]
-        public static extern bool SetForegroundWindow(IntPtr hWnd);
-        //
-
         private string status;
         private bool done = true;
         private int ticks;
@@ -65,6 +31,8 @@ namespace HorionInjector
         public MainWindow()
         {
             InitializeComponent();
+            VersionLabel.Content = "v" + GetVersion();
+            SetConnectionState(ConnectionState.NONE);
 
             Task.Run(() =>
             {
@@ -91,8 +59,36 @@ namespace HorionInjector
                 }
             });
 
-            if (!checkConnection())
+            if (!CheckConnection())
                 MessageBox.Show("Couldn't connect to download server. You can still inject a custom DLL.");
+        }
+
+        private void SetStatus(string status)
+        {
+            if (status == "done")
+                done = true;
+            else
+                this.status = status;
+        }
+
+        enum ConnectionState { NONE, CONNECTED, DISCONNECTED }
+        private void SetConnectionState(ConnectionState state)
+        {
+            switch (state)
+            {
+                case ConnectionState.NONE:
+                    ConnectionStateLabel.Content = "Not connected";
+                    ConnectionStateLabel.Foreground = System.Windows.Media.Brushes.SlateGray;
+                    break;
+                case ConnectionState.CONNECTED:
+                    ConnectionStateLabel.Content = "Connected";
+                    ConnectionStateLabel.Foreground = System.Windows.Media.Brushes.ForestGreen;
+                    break;
+                case ConnectionState.DISCONNECTED:
+                    ConnectionStateLabel.Content = "Disconnected";
+                    ConnectionStateLabel.Foreground = System.Windows.Media.Brushes.Coral;
+                    break;
+            }
         }
 
         private void InjectButton_Left(object sender, RoutedEventArgs e)
@@ -101,10 +97,10 @@ namespace HorionInjector
             done = false;
             
             status = "checking connection";
-            if (!checkConnection())
+            if (!CheckConnection())
             {
                 MessageBox.Show("Can't reach download server.");
-                done = true;
+                SetStatus("done");
                 return;
             }
 
@@ -130,125 +126,7 @@ namespace HorionInjector
                 done = true;
         }
 
-        private void Inject(string path)
-        {
-            if (!File.Exists(path))
-            {
-                MessageBox.Show("DLL not found, your Antivirus might have deleted it.");
-                done = true;
-                return;
-            }
-
-            if (File.ReadAllBytes(path).Length < 10)
-            {
-                MessageBox.Show("DLL broken (Less than 10 bytes)");
-                done = true;
-                return;
-            }
-
-
-            status = "setting file perms";
-            try
-            {
-                var fileInfo = new FileInfo(path);
-                var accessControl = fileInfo.GetAccessControl();
-                accessControl.AddAccessRule(new FileSystemAccessRule(new SecurityIdentifier("S-1-15-2-1"), FileSystemRights.FullControl, InheritanceFlags.None, PropagationFlags.NoPropagateInherit, AccessControlType.Allow));
-                fileInfo.SetAccessControl(accessControl);
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("Could not set permissions, try running the injector as admin.");
-                done = true;
-                return;
-            }
-
-            status = "finding process";
-            var processes = Process.GetProcessesByName("Minecraft.Windows");
-            if (processes.Length == 0)
-            {
-                status = "launching minecraft";
-                if (Interaction.Shell("explorer.exe shell:appsFolder\\Microsoft.MinecraftUWP_8wekyb3d8bbwe!App", Wait: false) == 0)
-                {
-                    MessageBox.Show("Failed to launch Minecraft (Is it installed?)");
-                    done = true;
-                    return;
-                }
-
-                Task.Run(() =>
-                {
-                    int t = 0;
-                    while (processes.Length == 0)
-                    {
-                        if (++t > 200)
-                        {
-                            MessageBox.Show("Minecraft launch took too long.");
-                            done = true;
-                            return;
-                        }
-
-                        processes = Process.GetProcessesByName("Minecraft.Windows");
-                        Thread.Sleep(10);
-                    }
-                    Thread.Sleep(100);
-                }).Wait();
-            }
-            var process = processes.OrderBy(p => p.StartTime).First();
-
-            for (int i = 0; i < process.Modules.Count; i++)
-            {
-                if (process.Modules[i].FileName == path)
-                {
-                    MessageBox.Show("Already injected!");
-                    done = true;
-                    return;
-                }
-            } 
-
-            status = "injecting into " + process.Id;
-            IntPtr handle = OpenProcess((IntPtr)2035711, false, (uint)process.Id);
-            if (handle == IntPtr.Zero || !process.Responding)
-            {
-                MessageBox.Show("Failed to get process handle");
-                done = true;
-                return;
-            }
-
-            IntPtr p1 = VirtualAllocEx(handle, IntPtr.Zero, (uint)(path.Length + 1), 12288U, 64U);
-            WriteProcessMemory(handle, p1, path.ToCharArray(), path.Length, out IntPtr p2);
-            IntPtr procAddress = GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
-            IntPtr p3 = CreateRemoteThread(handle, IntPtr.Zero, 0U, procAddress, p1, 0U, ref p2);
-            if (p3 == IntPtr.Zero)
-            {
-                MessageBox.Show("Failed to create remote thread");
-                done = true;
-                return;
-            }
-
-            uint n = WaitForSingleObject(p3, 5000);
-            if (n == 128L || n == 258L)
-            {
-                CloseHandle(p3);
-            }
-            else
-            {
-                VirtualFreeEx(handle, p1, 0, (IntPtr)32768);
-                if (p3 != IntPtr.Zero)
-                    CloseHandle(p3);
-                if (handle != IntPtr.Zero)
-                    CloseHandle(handle);
-            }
-
-            IntPtr windowH = FindWindow(null, "Minecraft");
-            if (windowH == IntPtr.Zero)
-                Console.WriteLine("Couldn't get window handle");
-            else
-                SetForegroundWindow(windowH);
-
-            status = "injected!";
-            done = true;
-        }
-
-        private bool checkConnection()
+        private bool CheckConnection()
         {
             try
             {
@@ -264,5 +142,7 @@ namespace HorionInjector
                 return false;
             }
         }
+
+        private string GetVersion() => Assembly.GetExecutingAssembly().GetName().Version.ToString(); 
     }
 }
